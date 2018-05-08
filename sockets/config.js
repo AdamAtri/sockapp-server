99e5b297b90b3a370295209c5b0ce27e45b784ab
adam <adam@atricoware.com>
2018-05-08 14:18:17 -0400

    Adds pending reasons.

--- sockets/config.js
+++ sockets/config.js
@@ -2,3 +2,8 @@
 module.exports.P_ACTIVE = 'active-players';
 module.exports.P_PENDING = 'pending-players';
 module.exports.T_ACTIVE = 'active-tables';
+module.exports.PENDING_REASONS = {
+  BUYIN: 'buy-in',
+  CASHOUT: 'cash-out',
+  DISPUTE: 'dispute'
+};
