a507c5c241f7e5a793d64f6726ed3474d9b14b6f
adam <adam@atricoware.com>
2018-05-11 18:16:49 -0400

    Adds eslint.

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = (function(){
+  const env = process.env.NODE_ENV;
+  return {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+      "linebreak-style": [
+          "error",
+          "unix"
+      ],
+      "semi": [
+          "error",
+          "always"
+      ],
+      "no-console": env === 'production' ? 2 : 1,
+    },
+    "globals": {
+      "io": false
+    }
+  }
+})();
