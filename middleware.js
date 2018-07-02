98b767ea89390019612431043a6aa9b1e81f0fba
adam <adam@atricoware.com>
2018-07-02 00:14:51 -0400

    Creates a middleware version of the app.

---
+++ middleware.js
@@ -0,0 +1,42 @@
+module.exports = function sbmiddleware(http) {
+
+  'use strict';
+
+  const express = require('express');
+  const sbApp = express();
+  const io = require('socket.io')(http);
+  const uuid = require('uuid/v1');
+
+  const { join } = require('path');
+
+  console.log('setting-up');
+  sbApp.use('*', (req, res, next) => {
+    if (! req.signedCookies.user ) {
+      let tempId = 'temp-' + uuid();
+      res.cookie('user', tempId, {
+        httpOnly: true,
+      });
+    }
+    next();
+  });
+
+
+  sbApp.use(express.static(join(__dirname, 'public')));
+  sbApp.use('/api', require('./api/index.js'));
+
+  const
+    playersIO = io.of('/players'), // make Player channel
+    dealersIO = io.of('/dealers'); // make Dealer channel
+
+  // Handle Player Sockets
+  const playerHandlers = require('./sockets/player-handler');
+  const applyPlayerHandlers = playerHandlers.bind(null, sbApp, playersIO, dealersIO);
+  playersIO.on('connection', applyPlayerHandlers);
+
+  // Handle Dealer Sockets
+  const dealerHandlers = require('./sockets/dealer-handler');
+  const applyDealerHandlers = dealerHandlers.bind(null, sbApp, playersIO, dealersIO);
+  dealersIO.on('connection', applyDealerHandlers);
+
+  return sbApp;
+};
